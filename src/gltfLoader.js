ebd5a8d0918a686b3d68a433bdcf12a035360a89
ndshen <andyshen1222@gmail.com>
2022-12-06 04:05:45 -0500

    introduce rxjs

--- src/gltfLoader.js
+++ src/gltfLoader.js
@@ -1,33 +1,53 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { BehaviorSubject, shareReplay, map, filter } from "rxjs";
 
 export const gltfAssetPath = {
-  STRAIGHT_ROAD: "roadAssets/straightroadPath/assets.gltf",
-  CROSS_ROAD: "roadAssets/Fourway cross gltf/CG-City Assets.gltf",
   SMALL_CAR: "vehicleAssets/small car 2 gltf/smallCar2.gltf",
   MINI_BUS: "vehicleAssets/minibus gltf/minibus.gltf",
+  ROAD: {
+    STRAIGHT_ROAD: "roadAssets/straightroadPath/assets.gltf",
+    CROSS_ROAD: "roadAssets/Fourway cross gltf/CG-City Assets.gltf",
+  },
 };
 
 export class gltfLoader {
   constructor() {
     this.loader = new GLTFLoader();
+    this.cache = new Map();
   }
 
-  load(filePath, callback) {
-    this.loader.load(
-      filePath,
-      (gltf) => {
-        console.log("success");
-        console.log(gltf);
-        callback(gltf.scene.children[0]);
-      },
-      (progress) => {
-        console.log("progress");
-        console.log(progress);
-      },
-      (error) => {
-        console.log("error");
-        console.log(error);
-      }
-    );
+  load(filePath) {
+    if (!this.cache.has(filePath)) {
+      const loaded$ = new BehaviorSubject(null);
+      this.cache[filePath] = loaded$.pipe(
+        filter(Boolean),
+        map(this.fetchObjectFromGLTF),
+        shareReplay(1)
+      );
+
+      this.loader.load(
+        filePath,
+        (gltf) => {
+          console.log("success");
+          console.log(gltf);
+
+          loaded$.next(gltf);
+        },
+        (progress) => {
+          console.log("progress");
+          console.log(progress);
+        },
+        (error) => {
+          console.log("error");
+          console.log(error);
+        }
+      );
+    }
+
+    return this.cache[filePath].asObservable();
+  }
+
+  fetchObjectFromGLTF(gltf) {
+    return gltf.scene.children[0];
   }
 }
