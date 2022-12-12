c68b22de1467d979a59e0354679392f043b9ee39
ndshen <andyshen1222@gmail.com>
2022-12-12 15:32:28 -0500

    add favicon

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -18,6 +18,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "../src/index.html"),
       minify: true,
+      favicon: path.resolve(__dirname, "../static/favicon.ico"),
     }),
     new MiniCSSExtractPlugin(),
   ],
